5c1431802a0311bb14b6f796be2cf1570723832a
monteirovictor <victormonteiromaia95@gmail.com>
2021-06-06 19:14:49 -0300

    Method save Fig.

--- ScatterPlot.py
+++ ScatterPlot.py
@@ -15,7 +15,11 @@ plt.xlabel(eixox)
 plt.ylabel(eixoy)
 
 #rodar
-plt.scatter(x,y,label="meus pontos",color="g",marker="^",s=100)
+plt.scatter(x,y,label="meus pontos",color="k",marker="^",s=100)
 plt.legend()
-plt.plot(x,y)
+plt.plot(x,y,color="k",linestyle="--")
+
 plt.show()
+
+#save figuras
+#plt.savefig("figura1.png")
