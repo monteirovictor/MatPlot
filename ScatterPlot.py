3300793d0bb5e6dfaeb73d9cb9b6a6d724b877f0
monteirovictor <victormonteiromaia95@gmail.com>
2021-06-06 19:03:56 -0300

    style ScatterPlot

---
+++ ScatterPlot.py
@@ -0,0 +1,21 @@
+from matplotlib import colors
+import matplotlib.pyplot as plt
+
+x=[1,2,3,4,5]
+y=[2,3,7,1,0]
+
+titulo="Graph Scatterplot"
+eixox="Eixo X"
+eixoy="Eixo y"
+
+
+#legendas
+plt.title(titulo)
+plt.xlabel(eixox)
+plt.ylabel(eixoy)
+
+#rodar
+plt.scatter(x,y,label="meus pontos",color="g",marker="^",s=100)
+plt.legend()
+plt.plot(x,y)
+plt.show()
